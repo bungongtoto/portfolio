8eb58d6ac7e062e1005e2040ca948b9b82a51606
bungongtoto <bungongkingsley@gmail.com>
2025-01-31 00:20:01 +0000

    home page responsive

--- recourses/script/main.js
+++ recourses/script/main.js
@@ -1,4 +1,30 @@
 const sectionImg = document.querySelector('.section-photo');
+const banner = document.getElementById('header-bottom');
+const bannerLeft = document.getElementById('banner-left');
+const bannerRight = document.getElementById('banner-right');
+const connectBtn = document.getElementById('connect-button');
+const newbannerText = document.createElement('p');
+
+// adding default styling class to the banner text
+newbannerText.classList.add('banner-text');
+newbannerText.innerHTML = "Hello, I Am Kingsley Ndzi Bungong"
+
+const updateLayout = () => {
+    if (window.innerWidth < 701) {
+        bannerLeft.style.display = "none";
+        bannerRight.style.display = "none";
+        banner.appendChild(connectBtn);
+        banner.prepend(newbannerText);
+    } else {
+        bannerLeft.style.display = "inline-block";
+        bannerRight.style.display = 'inline-block';
+        bannerRight.appendChild(connectBtn);
+
+        if ([... banner.children].includes(newbannerText)) {
+            banner.removeChild(newbannerText)
+        }
+    }
+}
 
 // changing section image grayscale on view
 const observer = new IntersectionObserver(entries => {
@@ -9,4 +35,6 @@ const observer = new IntersectionObserver(entries => {
     })
 }, {threshold: 0.9});
 
-observer.observe(sectionImg)
+observer.observe(sectionImg);
+window.onload = updateLayout;
+window.onresize = updateLayout;

37edbe1677c5a764ece064e53fe8ccd23c449d16
bungongtoto <bungongkingsley@gmail.com>
2025-06-15 21:10:21 +0100

    enable lets connect

--- recourses/script/main.js
+++ recourses/script/main.js
@@ -1,39 +1,63 @@
-const sectionImg = document.querySelector('.section-photo');
-const banner = document.getElementById('header-bottom');
-const bannerLeft = document.getElementById('banner-left');
-const bannerRight = document.getElementById('banner-right');
-const connectBtn = document.getElementById('connect-button');
-const newbannerText = document.createElement('p');
+const sectionImg = document.querySelector(".section-photo");
+const banner = document.getElementById("header-bottom");
+const bannerLeft = document.getElementById("banner-left");
+const bannerRight = document.getElementById("banner-right");
+const connectBtn = document.getElementById("connect-button");
+const newbannerText = document.createElement("p");
+const linkedIn = document.getElementById("linkedIn");
+const emailLink = document.getElementById("email");
 
 // adding default styling class to the banner text
-newbannerText.classList.add('banner-text');
-newbannerText.innerHTML = "Hello, I Am Kingsley Ndzi Bungong"
+newbannerText.classList.add("banner-text");
+newbannerText.innerHTML = "Hello, I Am Kingsley Ndzi Bungong";
+
+//navigating functions
+const navigateToLinkedIn = () => {
+  window.open("https://www.linkedin.com/in/kingsley-ndzi-bungong/", "_blank");
+};
+
+const naviagetToEmail = () => {
+  window.open(
+    "mailTo:bungongkingsley@gmail.com?subject=Want to connect",
+    "_blank"
+  );
+};
+
+//event listeners
+connectBtn.onclick = navigateToLinkedIn;
+
+linkedIn.onclick = navigateToLinkedIn;
+
+emailLink.onclick = naviagetToEmail;
 
 const updateLayout = () => {
-    if (window.innerWidth < 701) {
-        bannerLeft.style.display = "none";
-        bannerRight.style.display = "none";
-        banner.appendChild(connectBtn);
-        banner.prepend(newbannerText);
-    } else {
-        bannerLeft.style.display = "inline-block";
-        bannerRight.style.display = 'inline-block';
-        bannerRight.appendChild(connectBtn);
+  if (window.innerWidth < 701) {
+    bannerLeft.style.display = "none";
+    bannerRight.style.display = "none";
+    banner.appendChild(connectBtn);
+    banner.prepend(newbannerText);
+  } else {
+    bannerLeft.style.display = "inline-block";
+    bannerRight.style.display = "inline-block";
+    bannerRight.appendChild(connectBtn);
 
-        if ([... banner.children].includes(newbannerText)) {
-            banner.removeChild(newbannerText)
-        }
+    if ([...banner.children].includes(newbannerText)) {
+      banner.removeChild(newbannerText);
     }
-}
+  }
+};
 
 // changing section image grayscale on view
-const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        if(entry.isIntersecting){
-            entry.target.classList.add("inview")
-        }
-    })
-}, {threshold: 0.9});
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("inview");
+      }
+    });
+  },
+  { threshold: 0.9 }
+);
 
 observer.observe(sectionImg);
 window.onload = updateLayout;
--- recourses/script/project.js
+++ recourses/script/project.js
@@ -1,4 +1,3 @@
-console.log("hello")
 const viewProjectBtns = document.getElementsByClassName('project-button')
 
 
